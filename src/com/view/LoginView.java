20f26af81b61b7db6b426822e85fb2ce003673e5
Rafael Rodriguez <rafael.roguz@gmail.com>
2018-03-13 13:36:34 -0600

    V1 del login de usuario

--- src/com/view/LoginView.java
+++ src/com/view/LoginView.java
@@ -5,17 +5,18 @@ import javax.swing.*;
 public class LoginView extends javax.swing.JFrame {
 
     private javax.swing.JButton buttonLogin;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JTextField textUserName;
 
     public LoginView() {
         initComponents();
     }
 
-    @SuppressWarnings("unchecked")
     private void initComponents() {
 
         textUserName = new javax.swing.JTextField();
         buttonLogin = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Login");
@@ -26,42 +27,38 @@ public class LoginView extends javax.swing.JFrame {
         buttonLogin.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
         buttonLogin.setText("Login");
 
+        jLabel1.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        jLabel1.setText("Nombre de usuario");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addContainerGap(101, Short.MAX_VALUE)
-                                .addComponent(textUserName, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(102, 102, 102))
                         .addGroup(layout.createSequentialGroup()
                                 .addGap(136, 136, 136)
                                 .addComponent(buttonLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 80, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                                .addContainerGap(101, Short.MAX_VALUE)
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                        .addComponent(textUserName)
+                                        .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, 150, Short.MAX_VALUE))
+                                .addGap(102, 102, 102))
         );
         layout.setVerticalGroup(
                 layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                         .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                                .addGap(0, 69, Short.MAX_VALUE)
+                                .addGap(0, 41, Short.MAX_VALUE)
+                                .addComponent(jLabel1)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                                 .addComponent(textUserName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                                 .addGap(30, 30, 30)
                                 .addComponent(buttonLogin)
                                 .addGap(53, 53, 53))
         );
 
-        getRootPane().setDefaultButton(buttonLogin);
-        textUserName.requestFocus();
-
         pack();
-    }
-
-    public JButton getButtonLogin() {
-        return buttonLogin;
-    }
-
-    public JTextField getTextUserName() {
-        return textUserName;
-    }
+    }// </editor-fold>
 
     public static void main(String args[]) {
         try {
@@ -88,4 +85,11 @@ public class LoginView extends javax.swing.JFrame {
         });
     }
 
+    public JButton getButtonLogin() {
+        return buttonLogin;
+    }
+
+    public JTextField getTextUserName() {
+        return textUserName;
+    }
 }
